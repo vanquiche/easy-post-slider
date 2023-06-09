35f803e0ca421ae8d365a0c04825aad131885616
vanquiche <stevevang2010@hotmail.com>
2023-06-09 22:49:57 +0900

    feat: block in render markup

--- src/render.php
+++ src/render.php
@@ -1,8 +1,43 @@
 <?php
+
 /**
  * @see https://github.com/WordPress/gutenberg/blob/trunk/docs/reference-guides/block-api/block-metadata.md#render
  */
 ?>
-<p <?php echo get_block_wrapper_attributes(); ?>>
-	<?php esc_html_e( 'Parfait Designs Post Slider – hello from a dynamic block!', 'parfait-designs-post-slider' ); ?>
-</p>
+<div <?php echo get_block_wrapper_attributes(); ?>>
+	<small>post slider</small>
+	<?php
+	$args = array(
+		'post_type' => 'post',
+		'orderby' => 'date',
+		'order' => 'DESC',
+		'posts_per_page' => -1,
+		'tax_query' => array(
+			'relation' => 'OR',
+			array(
+				'taxonomy' => 'post_tag',
+				'field' => 'slug',
+				'terms' => $attributes['query']['tag_slug__in'],
+			),
+			array(
+				'taxonomy' => 'category',
+				'field' => 'id',
+				'terms' => $attributes['query']['cat'],
+			)
+		)
+	);
+	$query = new WP_Query($args);
+
+	if ($query->have_posts()) {
+		while ($query->have_posts()) {
+			$query->the_post();
+	?>
+			<p><?php echo the_title() ?></p>
+
+	<?php
+		}
+	} else {
+		echo '<p>no posts</p>';
+	}
+	?>
+</div>
