41797224ea5f0f270fd2b9935b536b45097ed0c2
vanquiche <stevevang2010@hotmail.com>
2023-06-09 14:27:22 +0900

    feat: add styling to editor controls

--- src/edit.js
+++ src/edit.js
@@ -64,161 +64,171 @@ export default function Edit({ attributes, setAttributes }) {
 	return (
 		<>
 			<InspectorControls>
-				<fieldset>
-					<h2>Content</h2>
-					<CheckboxControl
-						label="show title"
-						checked={attributes.content.showTitle}
-						onChange={(value) =>
-							setAttributes({
-								content: { ...attributes.content, showTitle: value },
-							})
-						}
-						help="Toggle post title visibility"
-					/>
-					<CheckboxControl
-						label="show excerpt"
-						checked={attributes.content.showExcerpt}
-						onChange={(value) =>
-							setAttributes({
-								content: { ...attributes.content, showExcerpt: value },
-							})
-						}
-						help="Toggle post excerpt visibility"
-					/>
-					<CheckboxControl
-						label="show link"
-						checked={attributes.content.showLink}
-						onChange={(value) =>
-							setAttributes({
-								content: { ...attributes.content, showLink: value },
-							})
-						}
-						help="Toggle post link visibility"
-					/>
-					<AlignmentMatrixControl
-						value={attributes.content.alignment.replace("-", " ")}
-						onChange={(value) =>
-							setAttributes({
-								content: {
-									...attributes.content,
-									alignment: value.replace(" ", "-"),
-								},
-							})
-						}
-					/>
-					<UnitControl
-						value={attributes.content.minHeight}
-						onChange={(value) =>
-							setAttributes({
-								content: { ...attributes.content, minHeight: value },
-							})
-						}
-						units={units}
-					/>
-				</fieldset>
-				<fieldset>
-					<h2>Styles</h2>
-					<label for="button-color-control">Button Color</label>
-					<ColorPalette
-						id="button-color-control"
-						colors={colors}
-						value={attributes.buttons.color}
-						onChange={(value) =>
-							setAttributes({
-								buttons: { ...attributes.buttons, color: value },
-							})
-						}
-					/>
-					<RangeControl
-						label="button opacity"
-						value={attributes.buttons.opacity}
-						onChange={(value) =>
-							setAttributes({
-								buttons: { ...attributes.buttons, opacity: parseInt(value) },
-							})
-						}
-						min={0}
-						max={100}
-						step={10}
-					/>
-					<label for="scrollbar-color-control">Scrollbar Color</label>
-					<ColorPalette
-						id="button-scrollbar-control"
-						colors={colors}
-						value={attributes.scrollbar.color}
-						onChange={(value) =>
-							setAttributes({
-								scrollbar: { ...attributes.scrollbar, color: value },
-							})
-						}
-					/>
-					<CheckboxControl
-						label="show scrollbar"
-						checked={attributes.scrollbar.showScrollbar}
-						onChange={(value) =>
-							setAttributes({
-								scrollbar: { ...attributes.scrollbar, showScrollbar: value },
-							})
-						}
-						help="Toggle scrollbar visibility"
-					/>
-					<RangeControl
-						label="cover image opacity"
-						value={attributes.coverImage.opacity}
-						onChange={(value) =>
-							setAttributes({
-								coverImage: {
-									...attributes.coverImage,
-									opacity: parseInt(value),
-								},
-							})
-						}
-						min={0}
-						max={100}
-						step={10}
-					/>
-				</fieldset>
-				<fieldset>
-					<h2>Query</h2>
-					<RangeControl
-						label="number of posts"
-						value={attributes.query.posts_per_page}
-						onChange={(value) =>
-							setAttributes({
-								query: { ...attributes.query, posts_per_page: parseInt(value) },
-							})
-						}
-						min={1}
-						max={6}
-						step={1}
-					/>
-					{categories && (
-						<SelectControl
-							label="filter by category"
-							value={attributes.query.cat}
-							options={categories.map(({ name, id }) => ({
-								label: name,
-								value: id,
-							}))}
+				<div className="editor-post-slider-controls">
+					<fieldset>
+						<h2>Content</h2>
+						<CheckboxControl
+							label="Show Title"
+							checked={attributes.content.showTitle}
 							onChange={(value) =>
 								setAttributes({
-									query: { ...attributes.query, cat: parseInt(value) },
+									content: { ...attributes.content, showTitle: value },
+								})
+							}
+							help="Toggle post title visibility"
+						/>
+						<CheckboxControl
+							label="Show Excerpt"
+							checked={attributes.content.showExcerpt}
+							onChange={(value) =>
+								setAttributes({
+									content: { ...attributes.content, showExcerpt: value },
+								})
+							}
+							help="Toggle post excerpt visibility"
+						/>
+						<CheckboxControl
+							label="Show Link"
+							checked={attributes.content.showLink}
+							onChange={(value) =>
+								setAttributes({
+									content: { ...attributes.content, showLink: value },
+								})
+							}
+							help="Toggle post link visibility"
+						/>
+						<AlignmentMatrixControl
+							value={attributes.content.alignment.replace("-", " ")}
+							onChange={(value) =>
+								setAttributes({
+									content: {
+										...attributes.content,
+										alignment: value.replace(" ", "-"),
+									},
 								})
 							}
 						/>
-					)}
-					<TextControl
-						label="filter by tag(s)"
-						value={attributes.query.tag_slug__in.join()}
-						onChange={(value) =>
-							setAttributes({
-								query: { ...attributes.query, tag_slug__in: value.split(",") },
-							})
-						}
-						help="Seperate tags by coma"
-					/>
-				</fieldset>
+						<UnitControl
+							value={attributes.content.minHeight}
+							onChange={(value) =>
+								setAttributes({
+									content: { ...attributes.content, minHeight: value },
+								})
+							}
+							units={units}
+						/>
+					</fieldset>
+					<fieldset>
+						<h2>Styles</h2>
+						<label for="button-color-control">Button Color</label>
+						<ColorPalette
+							style={{ marginTop: "12px" }}
+							id="button-color-control"
+							colors={colors}
+							value={attributes.buttons.color}
+							onChange={(value) =>
+								setAttributes({
+									buttons: { ...attributes.buttons, color: value },
+								})
+							}
+						/>
+						<RangeControl
+							label="button opacity"
+							value={attributes.buttons.opacity}
+							onChange={(value) =>
+								setAttributes({
+									buttons: { ...attributes.buttons, opacity: parseInt(value) },
+								})
+							}
+							min={0}
+							max={100}
+							step={10}
+						/>
+						<label for="scrollbar-color-control">Scrollbar Color</label>
+						<ColorPalette
+							style={{ marginTop: "12px" }}
+							id="button-scrollbar-control"
+							colors={colors}
+							value={attributes.scrollbar.color}
+							onChange={(value) =>
+								setAttributes({
+									scrollbar: { ...attributes.scrollbar, color: value },
+								})
+							}
+						/>
+						<CheckboxControl
+							label="show scrollbar"
+							checked={attributes.scrollbar.showScrollbar}
+							onChange={(value) =>
+								setAttributes({
+									scrollbar: { ...attributes.scrollbar, showScrollbar: value },
+								})
+							}
+							help="Toggle scrollbar visibility"
+						/>
+						<RangeControl
+							label="cover image opacity"
+							value={attributes.coverImage.opacity}
+							onChange={(value) =>
+								setAttributes({
+									coverImage: {
+										...attributes.coverImage,
+										opacity: parseInt(value),
+									},
+								})
+							}
+							min={0}
+							max={100}
+							step={10}
+						/>
+					</fieldset>
+					<fieldset>
+						<h2>Query</h2>
+						<RangeControl
+							label="number of posts"
+							value={attributes.query.posts_per_page}
+							onChange={(value) =>
+								setAttributes({
+									query: {
+										...attributes.query,
+										posts_per_page: parseInt(value),
+									},
+								})
+							}
+							min={1}
+							max={6}
+							step={1}
+						/>
+						{categories && (
+							<SelectControl
+								label="filter by category"
+								value={attributes.query.cat}
+								options={categories.map(({ name, id }) => ({
+									label: name,
+									value: id,
+								}))}
+								onChange={(value) =>
+									setAttributes({
+										query: { ...attributes.query, cat: parseInt(value) },
+									})
+								}
+							/>
+						)}
+						<TextControl
+							label="filter by tag(s)"
+							value={attributes.query.tag_slug__in.join()}
+							onChange={(value) =>
+								setAttributes({
+									query: {
+										...attributes.query,
+										tag_slug__in: value.split(","),
+									},
+								})
+							}
+							help="Seperate tags by coma"
+						/>
+					</fieldset>
+				</div>
 			</InspectorControls>
 			<div
 				{...useBlockProps({
@@ -231,22 +241,18 @@ export default function Edit({ attributes, setAttributes }) {
 				})}
 			>
 				{attributes.content.showTitle && (
-					<h2 className="post-slider__title">{attributes.content.showTitle}</h2>
+					<h2 className="slide__title">Post Title</h2>
 				)}
 				{attributes.content.showExcerpt && (
-					<p className="post-slider__excerpt">
-						{attributes.content.showExcerpt}
-					</p>
+					<p className="slide__excerpt">This is the Post Excerpt</p>
 				)}
 				{attributes.content.showLink && (
-					<div className="post-slider__link-container">
-						<a className="post-slider__link">
-							<span className="post-slider__link-label">
-								{attributes.content.showLink}
-							</span>
+					<div className="slide__link-container">
+						<a className="slide__link">
+							<span className="slide__link-label">Read More</span>
 							<span
 								id="button-overlay"
-								className="post-slider__link-overlay"
+								className="slide__link-overlay"
 								style={{ backgroundColor: attributes.buttons.color }}
 								aria-hidden
 							></span>
@@ -254,20 +260,16 @@ export default function Edit({ attributes, setAttributes }) {
 					</div>
 				)}
 				{attributes.scrollbar.showScrollbar && (
-					<div
-						id="post-slider-scrollbar"
-						className="post-slider__scrollbar"
-						aria-hidden
-					>
+					<div id="slide-scrollbar" className="slide__scrollbar" aria-hidden>
 						<div
-							className="post-slider__scrollbar-inner"
+							className="slide__scrollbar-inner"
 							style={{
 								backgroundColor: attributes.scrollbar.color,
 								width: "75%",
 							}}
 						></div>
 						<div
-							className="post-slider__scrollbar-inner"
+							className="slide__scrollbar-inner"
 							style={{
 								backgroundColor: attributes.scrollbar.color,
 								width: "100%",
@@ -277,29 +279,31 @@ export default function Edit({ attributes, setAttributes }) {
 					</div>
 				)}
 				<button
-					id="post-slider-left"
-					className="post-slider__navigation-button left"
+					id="slide-left"
+					className="slide__navigation-button left"
 					aria-label="previous slide"
 				>
-					<i className="caret-left" aria-hidden>
-						left
-					</i>
+					<i className="caret-left" aria-hidden></i>
 					<span
 						className="button-overlay"
-						style={{ backgroundColor: attributes.buttons.color }}
+						style={{
+							backgroundColor: attributes.buttons.color,
+							opacity: attributes.buttons.opacity + "%",
+						}}
 					></span>
 				</button>
 				<button
-					id="post-slider-right"
-					className="post-slider__navigation-button right"
+					id="slide-right"
+					className="slide__navigation-button right"
 					aria-label="next slide"
 				>
-					<i className="caret-right" aria-hidden>
-						right
-					</i>
+					<i className="caret-right" aria-hidden></i>
 					<span
 						className="button-overlay"
-						style={{ backgroundColor: attributes.buttons.color }}
+						style={{
+							backgroundColor: attributes.buttons.color,
+							opacity: attributes.buttons.opacity + "%",
+						}}
 					></span>
 				</button>
 			</div>
