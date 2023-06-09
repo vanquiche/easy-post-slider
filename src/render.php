2985ca988344cba9d60aba5a2bd7b4eb5722d5ac
vanquiche <stevevang2010@hotmail.com>
2023-06-09 09:43:53 +0900

    initial commit

---
+++ src/render.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * @see https://github.com/WordPress/gutenberg/blob/trunk/docs/reference-guides/block-api/block-metadata.md#render
+ */
+?>
+<p <?php echo get_block_wrapper_attributes(); ?>>
+	<?php esc_html_e( 'Parfait Designs Post Slider – hello from a dynamic block!', 'parfait-designs-post-slider' ); ?>
+</p>
