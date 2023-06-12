8f6b7461e21ee1bfe6cab69a1c87fab65ec5ff50
vanquiche <stevevang2010@hotmail.com>
2023-06-12 17:42:32 +0900

    feat: add styling to slider

--- src/render.php
+++ src/render.php
@@ -5,59 +5,65 @@
  */
 ?>
 <section <?php echo get_block_wrapper_attributes(); ?>>
+	<!-- <?php echo print_r($attributes) ?> -->
 	<?php
 	$args = array(
 		'post_type' => 'post',
 		'orderby' => 'date',
 		'order' => 'DESC',
 		'posts_per_page' => -1,
-		'tax_query' => array(
-			'relation' => 'OR',
-			array(
-				'taxonomy' => 'post_tag',
-				'field' => 'slug',
-				'terms' => $attributes['query']['tag_slug__in'],
-			),
-			array(
-				'taxonomy' => 'category',
-				'field' => 'id',
-				'terms' => $attributes['query']['cat'],
-			)
-		)
+		// 'tax_query' => array(
+		// 	'relation' => 'OR',
+		// 	array(
+		// 		'taxonomy' => 'post_tag',
+		// 		'field' => 'slug',
+		// 		'terms' => $attributes['query']['tag_slug__in'],
+		// 	),
+		// 	array(
+		// 		'taxonomy' => 'category',
+		// 		'field' => 'id',
+		// 		'terms' => $attributes['query']['cat'],
+		// 	)
+		// )
 	);
 	$query = new WP_Query($args);
 
 	if ($query->have_posts()) { ?>
-		<ul class='post-slider' aria-label='featured posts'>
-			<li class='navigation-button absolute-left'>
-				<button data-post-slider='navigation-button' aria-label='navigate previous slide'>
-					<i class='left-caret' aria-hidden></i>
-					<span class='overlay' aria-hidden></span>
+		<?php if ($query->post_count > 1) { ?>
+			<div class='interface top-50 left m-horizontal--16'>
+				<button class='navigation-button absolute-left' data-post-slider='navigation-button' data-post-slider-action='previous' aria-label='navigate previous slide'>
+					<i class='caret caret-left' style='background-color: <?php echo $attributes["buttons"]["fontColor"]; ?>;' aria-hidden='true'></i>
+					<span class='overlay' style='background-color: <?php echo $attributes["buttons"]["bgColor"] ?>;' aria-hidden></span>
 				</button>
-			</li>
-			<li class='navigation-button absolute-right'>
-				<button data-post-slider='navigation-button' aria-label='navigate next slide'>
-					<i class='right-caret' aria-hidden></i>
-					<span class='overlay' aria-hidden></span>
+			</div>
+			<div class='interface top-50 right m-horizontal--16'>
+				<button class='navigation-button absolute-right' data-post-slider='navigation-button' data-post-slider-action='next' aria-label='navigate next slide'>
+					<i class='caret caret-right' style='background-color: <?php echo $attributes["buttons"]["fontColor"]; ?>;' aria-hidden='true'></i>
+					<span class='overlay' style='background-color: <?php echo $attributes["buttons"]["bgColor"] ?>;' aria-hidden='true'></span>
 				</button>
-			</li>
+			</div>
+		<?php } ?>
+		<ul class='post-slider' style='min-height: <?php echo $attributes["content"]["minHeight"] ?>' aria-label='featured posts'>
+
 			<?php
 			while ($query->have_posts()) {
 				$query->the_post();
 			?>
-				<li class='slide'>
+				<li class='slide <?php echo 'align-content--' . $attributes["content"]["alignment"] ?>' style='left: <?php echo (($query->current_post) * 100) . "%" ?>;'>
 					<article class='slide-content'>
 						<h2 class='slide-content__title'>
 							<?php echo the_title() ?>
 						</h2>
 						<p class='slide-content__excerpt'>
-							<?php echo the_excerpt() ?>
+							<?php echo wp_strip_all_tags(get_the_excerpt(), true) ?>
 						</p>
 						<div class='slide-content__read-more'>
 							<a class='slide-content__read-more-link' href="<?php echo the_permalink() ?>">
-								Read Now
+								<span class='slide-content__read-more-label' style='color: <?php echo $attributes["buttons"]["fontColor"] ?>'>
+									Read Now
+								</span>
+								<span class='overlay' style='background-color: <?php echo $attributes["buttons"]["bgColor"] ?>;' aria-hidden='true'></span>
 							</a>
-							<span class='overlay' aria-hidden></span>
 						</div>
 					</article>
 				</li>
