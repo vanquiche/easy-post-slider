4cc59bae028c0ffc1e1f220bfb3cab94cfe2d09e
vanquiche <stevevang2010@hotmail.com>
2023-06-12 11:50:09 +0900

    feat: block in slider markup

--- src/render.php
+++ src/render.php
@@ -4,8 +4,7 @@
  * @see https://github.com/WordPress/gutenberg/blob/trunk/docs/reference-guides/block-api/block-metadata.md#render
  */
 ?>
-<div <?php echo get_block_wrapper_attributes(); ?>>
-	<small>post slider</small>
+<section <?php echo get_block_wrapper_attributes(); ?>>
 	<?php
 	$args = array(
 		'post_type' => 'post',
@@ -28,16 +27,44 @@
 	);
 	$query = new WP_Query($args);
 
-	if ($query->have_posts()) {
-		while ($query->have_posts()) {
-			$query->the_post();
-	?>
-			<p><?php echo the_title() ?></p>
-
+	if ($query->have_posts()) { ?>
+		<ul class='post-slider' aria-label='featured posts'>
+			<li class='navigation-button absolute-left'>
+				<button data-post-slider='navigation-button' aria-label='navigate previous slide'>
+					<i class='left-caret' aria-hidden></i>
+					<span class='overlay' aria-hidden></span>
+				</button>
+			</li>
+			<li class='navigation-button absolute-right'>
+				<button data-post-slider='navigation-button' aria-label='navigate next slide'>
+					<i class='right-caret' aria-hidden></i>
+					<span class='overlay' aria-hidden></span>
+				</button>
+			</li>
+			<?php
+			while ($query->have_posts()) {
+				$query->the_post();
+			?>
+				<li class='slide'>
+					<article class='slide-content'>
+						<h2 class='slide-content__title'>
+							<?php echo the_title() ?>
+						</h2>
+						<p class='slide-content__excerpt'>
+							<?php echo the_excerpt() ?>
+						</p>
+						<div class='slide-content__read-more'>
+							<a class='slide-content__read-more-link' href="<?php echo the_permalink() ?>">
+								Read Now
+							</a>
+							<span class='overlay' aria-hidden></span>
+						</div>
+					</article>
+				</li>
+			<?php
+			} ?>
+		</ul>
 	<?php
-		}
-	} else {
-		echo '<p>no posts</p>';
 	}
 	?>
-</div>
+</section>
