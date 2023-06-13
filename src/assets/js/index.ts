97d9f7fd18cc5f026da4bfcfed28d7c9c2a3878a
vanquiche <stevevang2010@hotmail.com>
2023-06-13 17:29:24 +0900

    refactor: rename variables

--- src/assets/js/index.ts
+++ src/assets/js/index.ts
@@ -2,14 +2,13 @@ import PostSlider from "./post-slider";
 
 document.addEventListener("DOMContentLoaded", () => {
 	const sliders = {};
-	const sliderBlocks = document.querySelectorAll(
+	const blocks = document.querySelectorAll(
 		".wp-block-parfait-designs-post-slider"
 	);
 
-	if (sliderBlocks) {
-		sliderBlocks.forEach((slide, i) => {
-			sliders[`slide${i}`] = new PostSlider(slide);
-			sliders[`slide${i}`].initSlider();
+	if (blocks) {
+		blocks.forEach((block, i) => {
+			sliders[`slide${i}`] = new PostSlider(block).initSlider();
 		});
 	}
 });
