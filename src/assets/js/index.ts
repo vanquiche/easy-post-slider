0defb2c57bd7654b33cf1c5d3a8873017537cfb1
vanquiche <stevevang2010@hotmail.com>
2023-06-30 14:41:35 +0900

    fix: update class query selector

--- src/assets/js/index.ts
+++ src/assets/js/index.ts
@@ -2,9 +2,7 @@ import PostSlider from './post-slider';
 
 document.addEventListener( 'DOMContentLoaded', () => {
 	const sliders = {};
-	const blocks = document.querySelectorAll(
-		'.wp-block-parfait-designs-post-slider'
-	);
+	const blocks = document.querySelectorAll( '.easy-post-slider-wrapper' );
 
 	if ( blocks ) {
 		blocks.forEach( ( block, i ) => {
