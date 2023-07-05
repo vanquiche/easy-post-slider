2e4514f9345de81ddfd105e1beebe2ef870ae6b8
vanquiche <stevevang2010@hotmail.com>
2023-07-05 12:41:53 +0900

    fix: set correct type assertion

--- src/assets/js/index.ts
+++ src/assets/js/index.ts
@@ -2,7 +2,9 @@ import PostSlider from './post-slider';
 
 document.addEventListener( 'DOMContentLoaded', () => {
 	const sliders = {};
-	const blocks = document.querySelectorAll( '.easy-post-slider-wrapper' );
+	const blocks = document.querySelectorAll(
+		'.easy-post-slider-wrapper'
+	) as NodeListOf< HTMLElement >;
 
 	if ( blocks ) {
 		blocks.forEach( ( block, i ) => {
