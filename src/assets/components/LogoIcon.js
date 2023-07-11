05a0a81edf30ef4d0b26083f344e7f869c096521
vanquiche <stevevang2010@hotmail.com>
2023-07-11 15:21:15 +0900

    feat: add parfait designs logo

---
+++ src/assets/components/LogoIcon.js
@@ -0,0 +1,71 @@
+const LogoIcon = ( props ) => (
+	<svg
+		width={ 512 }
+		height={ 512 }
+		viewBox="0 0 135.467 135.467"
+		xmlSpace="preserve"
+		xmlns="http://www.w3.org/2000/svg"
+		{ ...props }
+	>
+		<g
+			style={ {
+				opacity: 1,
+				fill: 'currentColor',
+				fillOpacity: 1,
+			} }
+			transform="translate(-21.564 -67.4) scale(.87858)"
+		>
+			<path
+				style={ {
+					opacity: 1,
+					fill: 'currentColor',
+					fillOpacity: 1,
+					strokeWidth: 0.174054,
+				} }
+				d="M63.139 83.047h77.075v1.959H63.139z"
+			/>
+			<path
+				style={ {
+					display: 'inline',
+					opacity: 1,
+					fill: 'currentColor',
+					fillOpacity: 1,
+					strokeWidth: 0.262373,
+				} }
+				d="M63.12 130.036v52.266s-.488 4.323 2.956 4.323h71.55s2.603 0 2.602-3.969l-.014-49.68s-15.42-10.126-41.117-1.306c-25.418 8.723-35.977-1.634-35.977-1.634z"
+			/>
+			<ellipse
+				style={ {
+					display: 'inline',
+					opacity: 1,
+					fill: 'currentColor',
+					fillOpacity: 1,
+					strokeWidth: 0.146653,
+				} }
+				cx={ 101.66 }
+				cy={ 199.848 }
+				rx={ 5.534 }
+				ry={ 5.348 }
+			/>
+			<path
+				style={ {
+					opacity: 1,
+					fill: 'currentColor',
+					fillOpacity: 1,
+					strokeWidth: 0.161965,
+				} }
+				d="M140.214 96.39v20.907s-10.28 9.146-38.547 1.153c-19.626-5.549-38.548 2.767-38.548 2.767V100.31s13.296-7.744 39.053-1.615c17.959 4.274 38.042-2.305 38.042-2.305z"
+			/>
+			<path
+				style={ {
+					opacity: 1,
+					fill: 'currentColor',
+					fillOpacity: 1,
+					strokeWidth: 0.156715,
+				} }
+				d="M101.61 212.528s-.634 2.046-4.812 2.613c-4.812.653-24.66 3.92-33.683 10.453h76.989c-9.022-6.533-28.87-9.8-33.683-10.453-4.177-.567-4.811-2.613-4.811-2.613z"
+			/>
+		</g>
+	</svg>
+);
+export default LogoIcon;
